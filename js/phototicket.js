63275478f79fce3c9bed0e0aa6d816716fdfd76d
juyeon-lee <janielee0919@domfam.com>
2020-05-08 16:03:41 +0900

    - 포토티켓 자랑하기 margin-bottom - 플립슬라이더 web/height 조정

--- js/phototicket.js
+++ js/phototicket.js
@@ -141,12 +141,12 @@
 
             // 초기화 토스트 팝업 --- 2020.05.07
             $('._reset').on('click',function () {
-                $('#filterToast').show().fadeOut(3000);
+                $('#filterToast').show().fadeOut(2000);
             })
 
             // 초기화 토스트 팝업 --- 2020.05.07
             $('._cancelPop').on('click',function () {
-                $('#cancelToast').show().fadeOut(3000);
+                $('#cancelToast').show().fadeOut(2000);
             })
 
             // 플립 슬라이더 팝업
